e166e5aae4a6dc380e90472f1b6f0a1f84c92810
Aquzi <pacerfan1998@gmail.com>
2022-03-09 01:31:46 -0500

    seperated mapping

--- src/App.js
+++ src/App.js
@@ -10,24 +10,26 @@ export default function App() {
     const feed = new window.DOMParser().parseFromString(contents, "text/xml");
     const books = feed.querySelectorAll("item");
     const feedBooks = [...books].map((el) => ({
-      bookID: el.querySelector("book_id").innerHTML,
+      bookId: el.querySelector("book_id").innerHTML,
       author: el.querySelector("author_name").innerHTML,
     }));
     setBooks(feedBooks);
   };
 
   getFeed();
+  
+  const library = books.map((book) => {
+    return (
+      <div>
+        <h1>{book.author}</h1>
+        <h1>{book.bookId}</h1>
+      </div>
+    )
+  })
 
   return (
     <div className="App">
-      {books.map((book) => {
-        return (
-          <div>
-            <h1>{book.bookID}</h1>
-            <h1>{book.author}</h1>
-          </div>
-        );
-      })}
+      {library}
     </div>
   );
 }
